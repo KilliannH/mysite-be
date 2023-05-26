7fbb1ea4fdef77922076701fe03c20cd97a3c68d
KilliannH <killiannhervagault@gmail.com>
2023-05-26 16:15:04 +0200

    auth flow

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'polls.apps.PollsConfig',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+    # by default django want's to login with username + password
+    REQUIRED_FIELDS = ['password', 'username']
+
+
+# 16:22 todo -- check requiredFileds in the doc (for me those fields are logical)
